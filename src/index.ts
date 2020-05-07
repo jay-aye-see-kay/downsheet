003341469d468206b00b2c1d496fe4fcd5f85002
Jack Rose <user@jackrose.co.nz>
2020-05-07 21:43:32 +1000

    just works with comment labels

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
-// const TOML = require('@iarna/toml')
 import Toml from '@iarna/toml';
+
 
 type Cell = string | number;
 type Data = Cell[][];
+
 
 const getColumnCount = (data: Data): number => {
   let max = 0;
@@ -11,6 +12,7 @@ const getColumnCount = (data: Data): number => {
   }
   return max + 1; // counts are not 0 indexed
 }
+
 
 const cellToString = (cell: Cell): string => {
   switch(typeof cell) {
@@ -23,26 +25,44 @@ const cellToString = (cell: Cell): string => {
   }
 }
 
+
 const getColumnWidths = (data: Data) =>
   data.map(row =>
     Math.max(...row.map(cell => cellToString(cell).length))
   )
+
+
+// TODO handle more than 26 cols
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+const idxToLetter = (i: number) => alphabet[i];
+
+
+const makeTopLabels = (columnWidths: number[]): string => {
+  const labels = columnWidths.map((width, colIdx) => {
+    const letter = idxToLetter(colIdx);
+    const padding = " ".repeat(width - letter.length);
+    return ` ${letter}${padding}`;
+  })
+  const labelRow = "  #" + labels.join(' ');;
+  return `${labelRow}\n`;
+}
+
 
 const formatDataAttribute = (data: Data): string => {
   const columnCount = getColumnCount(data);
   const columnWidths = getColumnWidths(data);
 
   let outString = "data = [\n";
-
+  outString += makeTopLabels(columnWidths);
   data.forEach((row, rowIdx) => {
     const rowAsString = row.map(cell => cellToString(cell)).join(", ");
-    outString += `  [ ${rowAsString} ],\n`;
+    outString += `  [ ${rowAsString} ], # ${rowIdx + 1}\n`;
   });
 
   outString += "]\n";
-
   return outString;
 }
+
 
 export const formatDownsheet = (file: string): string => {
   const fileAsJson = Toml.parse(file);
