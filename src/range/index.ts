cdf7a4114f6fd2791db9c09c5425a7e5fbc15838
Jack Rose <user@jackrose.co.nz>
2020-05-09 17:44:11 +1000

    create `Range` for parsing and resolving ranges

---
+++ src/range/index.ts
@@ -0,0 +1,110 @@
+const char = "([a-zA-Z])+";
+const num = "([0-9])+";
+
+const valid = {
+  cell: new RegExp(`^${char}${num}$`),
+  array: new RegExp(`^${char}${num}:${char}${num}$`),
+  row: new RegExp(`^${num}:${num}$`),
+  column: new RegExp(`^${char}:${char}$`),
+};
+
+type BoundedRangeKind =
+  | "cell"
+  | "array"
+  | "matrix"
+
+// needs to be resolved into a bounded range
+type UnboundedRangeKind =
+  | "row"
+  | "column"
+  | "row matrix"
+  | "column matrix";
+
+type RangeKind =
+  | BoundedRangeKind
+  | UnboundedRangeKind
+  | "invalid";
+
+type CellKind =
+  | 'none'
+  | 'string'
+  | 'int'
+  | 'float'
+  | 'boolean'
+  | 'offset-date-time'
+  | 'date-time'
+  | 'date'
+  | 'time';
+
+
+// Position is a A1/ZA199 syntax string by users
+type Position = string;
+
+// Coord is [0, 0] syntax, 0 indexed
+type Coord = [number, number];
+
+type Cell = {
+  kind: CellKind;
+  value: any; // TODO
+};
+
+type SheetArray = Cell[];
+type SheetMatrix = Cell[][];
+type SheetData = SheetMatrix;
+
+class RangeError extends Error {}
+
+
+const kind = (rangeString: string): RangeKind => {
+  if (valid.cell.test(rangeString)) return 'cell';
+  if (valid.array.test(rangeString)) return 'array';
+  // TODO matrix vs array
+  if (valid.row.test(rangeString)) return 'row';
+  if (valid.column.test(rangeString)) return 'column';
+  // TODO row vs rows and column vs columns
+
+  return 'invalid';
+}
+
+// this probably need better tests, I'm not confident it handles large numbers quite right
+const columnStringToIndex = (columnString: string): number => {
+  const charValue = columnString.toUpperCase().split('').reverse().map((char, index) => {
+    const digitValue = char.charCodeAt(0) - "A".charCodeAt(0) + 1;
+    if (digitValue < 1 && digitValue > 26) {
+      throw new Error('non-ascii alphabet column provided');
+    }
+    return digitValue * (26 ** index);
+  });
+  return charValue.reduce((sum, v) => sum + v, 0) - 1;
+}
+
+
+const positionToCoord = (position: Position): Coord => {
+  const columnMatches = position.match(/[a-zA-Z]+/);
+  const columnIndex = columnMatches && columnMatches[0] ?
+    columnStringToIndex(columnMatches[0])
+    : 0;
+
+  const rowMatches = position.match(/[0-9]+/);
+  const rowIndex = rowMatches && rowMatches[0]
+    ? parseInt(rowMatches[0], 10) - 1
+    : 0;
+
+  return [columnIndex, rowIndex];
+}
+
+
+const resolve = (rangeString: string, sheetData: SheetData): Cell | SheetArray | SheetMatrix => {
+  const kind = Range.kind(rangeString);
+  if (kind === 'invalid') throw new RangeError();
+  if (kind === 'row'
+   || kind === 'row matrix'
+   || kind === 'column'
+   || kind === 'column matrix'
+   ) { /* TODO handle unbounded range */ }
+
+   return sheetData;
+}
+
+
+export const Range = { kind, positionToCoord, columnStringToIndex };
---
+++ src/range/range.spec.ts
@@ -0,0 +1,58 @@
+import { equal, deepStrictEqual } from "assert";
+
+import { Range } from "./index";
+
+
+describe('Range module', () => {
+  describe('kind()', () => {
+    it('should return invalid with no input', () => {
+      equal(Range.kind(''), 'invalid');
+    });
+    it('should return true for a cell', () => {
+      equal(Range.kind('A1'), 'cell');
+    });
+    it('should return array for an array', () => {
+      equal(Range.kind('A1:A3'), 'array');
+    });
+    it('should return row for a row', () => {
+      equal(Range.kind('1:1'), 'row');
+    });
+    it('should return column for a column', () => {
+      equal(Range.kind('A:A'), 'column');
+    });
+  });
+
+  describe('columnStringToIndex()', () => {
+    it('should convert A to 0', () => {
+      equal(Range.columnStringToIndex('A'), 0);
+    });
+    it('should convert Z to 25', () => {
+      equal(Range.columnStringToIndex('Z'), 25);
+    });
+    it('should convert AA to 26', () => {
+      equal(Range.columnStringToIndex('AA'), 26);
+    });
+    it('should convert AZ to 51', () => {
+      equal(Range.columnStringToIndex('AZ'), 51);
+    });
+    it('should convert ZZ to 701', () => {
+      equal(Range.columnStringToIndex('ZZ'), 701);
+    });
+    it('should also handle lower case', () => {
+      equal(Range.columnStringToIndex('a'), 0);
+      equal(Range.columnStringToIndex('zz'), 701);
+    });
+  });
+
+  describe('positionToCoord()', () => {
+    it('should handle a simple position a A1', () => {
+      deepStrictEqual(Range.positionToCoord('A1'), [0, 0]);
+    });
+    it('should handle a position at Z99', () => {
+      deepStrictEqual(Range.positionToCoord('Z99'), [25, 98]);
+    });
+    it('should handle a position at BA1000', () => {
+      deepStrictEqual(Range.positionToCoord('BA1000'), [52, 999]);
+    });
+  });
+});
