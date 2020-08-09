9ef32d533f35ff744e3bf634e6785f2a9396e949
Jack Rose <user@jackrose.co.nz>
2020-08-09 17:29:21 +1000

    inline labels feature

--- dsc/src/calc/index.ts
+++ dsc/src/calc/index.ts
@@ -2,9 +2,32 @@ import { SheetFile } from '../types';
 
 import { evalFormula } from './parser';
 import { Range } from "../range";
+import { toSb26 } from '../hexavigesimal';
 
-// Take a SheetFile and run over all formula, updating the cell data
-// Note: This doesn't make a graph so can't detect circular dependancies or even calculate in the correct order, TODO
+const getInlineLabels = (sheetFile: SheetFile): SheetFile['labels'] => {
+  const inlineLabels: SheetFile['labels'] = {};
+
+  sheetFile.data.forEach((row, rowIdx) => {
+    row.forEach((cell, colIdx) => {
+      if (cell.kind !== "string") return;
+
+      const { value } = cell;
+      if (value.endsWith(":") && value.length > 1 && !value.includes(" ")) {
+        const labelName = value.substring(0, value.length - 1);
+        // label refers one to the right (but rows are 0 indexed)
+        inlineLabels[labelName] = `${toSb26(colIdx + 1)}${rowIdx + 1}`;
+      }
+    });
+  });
+
+  return inlineLabels;
+};
+
+/**
+ * Take a SheetFile and run over all formula, updating the cell data
+ *
+ * Note: This doesn't make a graph so can't detect circular dependancies or even calculate in the correct order, TODO
+ */
 export const calc = (sheetFile: SheetFile): SheetFile => {
   if (!sheetFile.formula) return sheetFile;
 
@@ -14,20 +37,25 @@ export const calc = (sheetFile: SheetFile): SheetFile => {
   // - I should probably just ignore labels that match the isRange regex
   // - and put a warning in too, one day obvs
 
-  if (sheetFile.labels && sheetFile.formula) {
+  const labels: SheetFile['labels'] = { ...sheetFile.labels, ...getInlineLabels(sheetFile) };
+  const hasLabels = !!Object.keys(labels).length;
+
+  if (hasLabels && sheetFile.formula) {
     // dumb find and replace of labels
     const newFormula: SheetFile['formula'] = {};
-    const oldFormula = Object.entries(sheetFile.formula);
-    const oldLabels = Object.entries(sheetFile.labels);
+    const formulaPairs = Object.entries(sheetFile.formula);
+    const labelPairs = Object.entries(labels);
 
-    for (const [formulaRange, formulaValue] of oldFormula) {
-      for (const [labelKey, labelValue] of oldLabels) {
+    for (const [formulaRange, formulaValue] of formulaPairs) {
+      let newFormulaRange = formulaRange;
+      let newFormulaValue = formulaValue;
+      for (const [labelKey, labelValue] of labelPairs) {
         // ignore matches followed by an open bracket, those are functions
         // TODO write tests
-        const newFormulaRange = formulaRange.replace(new RegExp(labelKey + '(?!\\()', 'g'), labelValue);
-        const newFormulaValue = formulaValue.replace(new RegExp(labelKey + '(?!\\()', 'g'), labelValue);
-        newFormula[newFormulaRange] = newFormulaValue;
+        newFormulaRange = newFormulaRange.replace(new RegExp(labelKey + '(?!\\()', 'g'), labelValue);
+        newFormulaValue = newFormulaValue.replace(new RegExp(labelKey + '(?!\\()', 'g'), labelValue);
       }
+      newFormula[newFormulaRange] = newFormulaValue;
     }
     // put the substituted formula into the new file
     newSheetFile.formula = newFormula;
